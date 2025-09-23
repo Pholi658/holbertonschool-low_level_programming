09e9bd03ced78c6cd0c129d26d0738c28fdd4b80
Pholiso Toti <84088467+Pholi658@users.noreply.github.com>
2025-09-23 16:36:05 +0200

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+*main - this is the beginning of our function
+*return - this is the end of our function
+*/
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }

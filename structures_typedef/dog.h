8a98c93c0d8ee3f62fe0c6379a67376930c6f658
Pholi658 <totipholiso@gmail.com>
2025-10-03 12:04:57 +0200

    structures commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *struct dog - structure
+ *@name: name of dog
+ *@age: age of owner
+ *@owner: name of dog ownet
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */

f4e193bf6ba8a90898d3308fcc939b45e22dd421
Pholi658 <totipholiso@gmail.com>
2025-10-28 12:24:50 +0200

    corrections

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,10 +4,9 @@
 #include <stdio.h>
 /**
  *main - this is the beginning of a function
- *
  *Return: this is the end of the function
  */
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 int n;
@@ -15,12 +14,10 @@ int last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
 last_digit = n % 10;
 if (last_digit > 5)
 {
-printf("Last digit of, %d is %d and is greater than 5\n",
-n, last_digit);
+printf("Last digit of, %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit == 0)
 {
@@ -28,8 +25,7 @@ printf("Last digit of,%d is %d and is 0\n", n, last_digit);
 }
 else
 {
-printf("Last digit of, %d is %d and is less than 6 and not 0\n",
-n, last_digit);
+printf("Last digit of, %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 return (0);
 }
--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  *main - this is the beggining of the function
- *
  *Return: this the end of the function
  */
 
@@ -10,8 +9,9 @@ int main(void)
 int n = 0;
 while (n < 10)
 {
-printf("%d\n", n);
+printf("%d", n);
 n++;
 }
+printf("\n");
 return (0);
 }

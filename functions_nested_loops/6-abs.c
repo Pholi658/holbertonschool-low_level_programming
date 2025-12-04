5645e33dc552afed1d7073520a7268f301a78dbe
Pholi658 <totipholiso@gmail.com>
2025-12-04 11:57:23 +0200

    functions and nested loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_abs- this is the definition of the function.
+ *@int: this is the parameter for the value.
+ *Return: This is the end of the function.
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+return (n);
+}

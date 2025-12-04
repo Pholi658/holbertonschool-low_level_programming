a206ea49231e52ca76a37cb5a3b254d3f7837a90
Pholi658 <totipholiso@gmail.com>
2025-12-04 16:48:43 +0200

    more functions and ..

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *_isupper- beginning of the function.
+ *
+ *@c: the value.
+ *
+ *Return- returns 0.
+ */
+
+int _isupper(int c)
+{
+if(c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
